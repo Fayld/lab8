a7fd1f8c918fc4203898f45e79e28d3c2cee96c5
Алексей Волков <aleks3204932@mail.ru>
2023-09-30 11:12:28 +0300

    Время

---
+++ time.pas
@@ -0,0 +1,14 @@
+﻿begin
+  var year:=readinteger('Введите год :');
+  if (year.Divs(4))and not ((year.Divs(100))and (year.Divs(400)))then
+    print('366  - Високосный')
+  else
+    print('365 - Невисокосный');
+end.
+
+//Введите год : 2012
+//366  - Високосный 
+
+
+//Введите год : 2023
+//365 - Невисокосный 
